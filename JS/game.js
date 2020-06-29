1e6481f0a9d85f2412aea029d0d833f5f649eec0
RoyalFang <GAMihov18@codingburgas.bg>
2020-06-29 14:59:16 +0300

    Try to make the things work

--- JS/game.js
+++ JS/game.js
@@ -2,9 +2,47 @@
 let teamnumber;
 let turn;
 let move;
+const areas={
+  area1: document.getElementById(area1),
+  area2: document.getElementById(area2),
+  area3: document.getElementById(area3),
+  area4: document.getElementById(area4),
+  area5: document.getElementById(area5),
+  area6: document.getElementById(area6),
+  area7: document.getElementById(area7),
+  area8: document.getElementById(area8),
+  area9: document.getElementById(area9),
+  area10: document.getElementById(area10),
+  area11: document.getElementById(area11),
+  area12: document.getElementById(area12),
+  area13: document.getElementById(area13),
+  area14: document.getElementById(area14),
+  area15: document.getElementById(area15),
+  area16: document.getElementById(area16),
+  area17: document.getElementById(area17),
+  area18: document.getElementById(area18),
+  area19: document.getElementById(area19),
+  area20: document.getElementById(area20),
+  area21: document.getElementById(area21),
+  area22: document.getElementById(area22),
+  area23: document.getElementById(area23),
+  area24: document.getElementById(area24),
+  area25: document.getElementById(area25),
+  area26: document.getElementById(area26),
+  area27: document.getElementById(area27),
+  area28: document.getElementById(area28),
+  area29: document.getElementById(area29),
+  area30: document.getElementById(area30),
+  area31: document.getElementById(area31),
+  area32: document.getElementById(area32),
+  area33: document.getElementById(area33),
+  area34: document.getElementById(area34),
+  area35: document.getElementById(area35),
+  area36: document.getElementById(area36),
+}
 
-
-
+let areasArray = Array.from(areas);
+console.log(areasArray);
 //Functions
 
 
