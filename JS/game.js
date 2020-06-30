522f8bbf5e486e84d18b96cd5ea45ef53bfe6635
RoyalFang <GAMihov18@codingburgas.bg>
2020-06-30 12:15:56 +0300

    Add spawnareas and more buttons

--- JS/game.js
+++ JS/game.js
@@ -6,6 +6,7 @@ const infoTurn=document.getElementById('info-turnPerson');
 const infoTurnAmount=document.getElementById('info-turnAmount');
 const dice = document.getElementById('die');
 let rollDie = document.getElementById('roll-button').addEventListener('click',rollDice);
+let resetGameButton= document.getElementById('reset-button').addEventListener('click',resetGame);
 const areas=[
   area1= document.getElementById('area1'),
   area2= document.getElementById('area2'),
@@ -52,21 +53,25 @@ const memory={
       position:0,
       isOut:false,
       hasBeenOnSpawn:false,
+      pawnSpawnPoint: document.getElementById('spawn-area1')
     },
     pawn2:{
       position:0,
       isOut:false,
       hasBeenOnSpawn:false,
+      pawnSpawnPoint: document.getElementById('spawn-area2')
     },
     pawn3:{
       position:0,
       isOut:false,
       hasBeenOnSpawn:false,
+      pawnSpawnPoint: document.getElementById('spawn-area3')
     },
     pawn4:{
       position:0,
       isOut:false,
       hasBeenOnSpawn:false,
+      pawnSpawnPoint: document.getElementById('spawn-area4')
     },
   },
   player2:{
@@ -76,21 +81,25 @@ const memory={
       position:0,
       isOut:false,
       hasBeenOnSpawn:false,
+      pawnSpawnPoint: document.getElementById('spawn-area5')
     },
     pawn2:{
       position:0,
       isOut:false,
       hasBeenOnSpawn:false,
+      pawnSpawnPoint: document.getElementById('spawn-area6')
     },
     pawn3:{
       position:0,
       isOut:false,
       hasBeenOnSpawn:false,
+      pawnSpawnPoint: document.getElementById('spawn-area7')
     },
     pawn4:{
       position:0,
       isOut:false,
       hasBeenOnSpawn:false,
+      pawnSpawnPoint: document.getElementById('spawn-area8')
     },
   },
   player3:{
@@ -100,21 +109,25 @@ const memory={
       position:0,
       isOut:false,
       hasBeenOnSpawn:false,
+      pawnSpawnPoint: document.getElementById('spawn-area9')
     },
     pawn2:{
       position:0,
       isOut:false,
       hasBeenOnSpawn:false,
+      pawnSpawnPoint: document.getElementById('spawn-area10')
     },
     pawn3:{
       position:0,
       isOut:false,
       hasBeenOnSpawn:false,
+      pawnSpawnPoint: document.getElementById('spawn-area11')
     },
     pawn4:{
       position:0,
       isOut:false,
       hasBeenOnSpawn:false,
+      pawnSpawnPoint: document.getElementById('spawn-area12')
     },
   },
   player4:{
@@ -124,21 +137,25 @@ const memory={
       position:0,
       isOut:false,
       hasBeenOnSpawn:false,
+      pawnSpawnPoint: document.getElementById('spawn-area13')
     },
     pawn2:{
       position:0,
       isOut:false,
       hasBeenOnSpawn:false,
+      pawnSpawnPoint: document.getElementById('spawn-area14')
     },
     pawn3:{
       position:0,
       isOut:false,
       hasBeenOnSpawn:false,
+      pawnSpawnPoint: document.getElementById('spawn-area15')
     },
     pawn4:{
       position:0,
       isOut:false,
       hasBeenOnSpawn:false,
+      pawnSpawnPoint: document.getElementById('spawn-area16')
     },
   },
 };
@@ -146,10 +163,10 @@ const memory={
 console.log(memory);
 
 //Code
-hideLoad();
-
+hideLoaded();
+loadSpawnPoints();
 //Functions
-function hideLoad(){
+function hideLoaded(){
   areas.forEach(element =>{
     element.classList='hidden';
   })
@@ -161,27 +178,15 @@ function randomNumber(min, max) {
 //check which team is something
 function checkTeam(teamnumber){
   if (teamnumber===1) {
-    areas.forEach(element => {
-      element.classList='B-visible';
-    });
     infoTurn.innerHTML=`<span style="color: white;">Belle's Turn</span>`;
   }
   if (teamnumber===2) {
-    areas.forEach(element => {
-      element.classList='R-visible';
-    });
     infoTurn.innerHTML=`<span style="color: white;">Robert's Turn</span>`;
   }
   if (teamnumber===3) {
-    areas.forEach(element => {
-      element.classList='U-visible';
-    });
     infoTurn.innerHTML=`<span style="color: white;">Ursule's Turn</span>`;
   }
   if (teamnumber===4) {
-    areas.forEach(element => {
-      element.classList='H-visible';
-    });
     infoTurn.innerHTML=`<span style="color: white;">Harry's Turn</span>`;
   }
 }
@@ -212,7 +217,61 @@ function rollDice() {
     checkTeam(teamnumber);
     turn++;
     infoTurnAmount.innerHTML=`Turn: ${turn}`;
+    checkPerfectRole(rand,teamnumber);
 }
-
+function loadSpawnPoints(){
+  memory.player1.pawn1.pawnSpawnPoint.classList='B-visible';
+  memory.player1.pawn2.pawnSpawnPoint.classList='B-visible';
+  memory.player1.pawn3.pawnSpawnPoint.classList='B-visible';
+  memory.player1.pawn4.pawnSpawnPoint.classList='B-visible';
+  memory.player2.pawn1.pawnSpawnPoint.classList='R-visible';
+  memory.player2.pawn2.pawnSpawnPoint.classList='R-visible';
+  memory.player2.pawn3.pawnSpawnPoint.classList='R-visible';
+  memory.player2.pawn4.pawnSpawnPoint.classList='R-visible';
+  memory.player3.pawn1.pawnSpawnPoint.classList='U-visible';
+  memory.player3.pawn2.pawnSpawnPoint.classList='U-visible';
+  memory.player3.pawn3.pawnSpawnPoint.classList='U-visible';
+  memory.player3.pawn4.pawnSpawnPoint.classList='U-visible';
+  memory.player4.pawn1.pawnSpawnPoint.classList='H-visible';
+  memory.player4.pawn2.pawnSpawnPoint.classList='H-visible';
+  memory.player4.pawn3.pawnSpawnPoint.classList='H-visible';
+  memory.player4.pawn4.pawnSpawnPoint.classList='H-visible';
+}
+function resetGame(){
+  hideLoaded();
+  loadSpawnPoints();
+  turn=0;
+  teamnumber=0;
+  infoTurnAmount.innerHTML='';
+  infoTurn.innerHTML='';
+}
+function movePawns(rollnumber,team) {
+  for (let i = 0; i < rollnumber; i++) {
+    
+    
+  }
+}
+function checkPerfectRole(rollNumber,team){
+  if(rollNumber===6){
+    if (team==1) {
+      if (memory.player1.pawn1.isOut==false) {
+        memory.player1.pawn1.isOut=true;
+        memory.player1.pawn1.position=1;
+      }
+      if (memory.player1.pawn2.isOut==false) {
+        memory.player1.pawn2.isOut=true;
+        memory.player1.pawn2.position=1;
+      }
+      if (memory.player1.pawn3.isOut==false) {
+        memory.player1.pawn3.isOut=true;
+        memory.player1.pawn3.position=1;
+      }
+      if (memory.player1.pawn4.isOut==false) {
+        memory.player1.pawn4.isOut=true;
+        memory.player1.pawn4.position=1;
+      }
+    }
+  }
+}
 //Logs 0 if reach here
 console.log(0);
